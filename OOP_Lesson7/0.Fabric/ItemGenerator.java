e8dab293d634622188bba6202ef8cfd47a34eb37
MrVoropaev <mrvoropaev@gmail.com>
2023-05-25 11:03:18 +0300

    OOP_Lesson7

---
+++ OOP_Lesson7/0.Fabric/ItemGenerator.java
@@ -0,0 +1,11 @@
+
+
+public abstract class ItemGenerator {
+    public void openRerward()
+    {
+        iGameItem gameItem = createItem();
+        gameItem.open();
+    } 
+
+    public abstract iGameItem createItem();
+}
