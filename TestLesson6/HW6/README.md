32e2de7935c73c827c528b4ae11120a19bbc3ab3
MrVoropaev <mrvoropaev@gmail.com>
2023-10-24 22:30:15 +0300

    TestLesson6

---
+++ TestLesson6/HW6/README.md
@@ -0,0 +1,2 @@
+Описание и результат тестов https://docs.google.com/document/d/1l43au7jjhEyIkWWiMAiVd6OAA7tEVnz1h2Cq6ok7XpI/edit?usp=sharing
+
---
+++ TestLesson6/HW6/test_list_comparator.py
@@ -0,0 +1,29 @@
+import unittest
+from list_comparator import ListComparator
+
+class test_list_comparator(unittest.TestCase):
+
+    def test_compare_lists_with_different_averages(self):
+        list1 = [1, 2, 3, 4, 5]
+        list2 = [6, 7, 8, 9, 10]
+        comparator = ListComparator(list1, list2)
+        assert comparator.compare_lists() == "Второй список имеет большее среднее значение"
+
+    def test_compare_lists_with_same_averages(self):
+        list1 = [1, 2, 3]
+        list2 = [4, 5, 6]
+        comparator = ListComparator(list1, list2)
+        assert comparator.compare_lists() == "Средние значения равны"
+
+    def test_compare_lists_with_empty_lists(self):
+        list1 = []
+        list2 = []
+        comparator = ListComparator(list1, list2)
+        assert comparator.compare_lists() == "Средние значения равны"
+
+    def test_compare_lists_with_one_empty_list(self):
+        list1 = [1, 2, 3]
+        list2 = []
+        comparator = ListComparator(list1, list2)
+        assert comparator.compare_lists() == "Первый список имеет большее среднее значение"
+
